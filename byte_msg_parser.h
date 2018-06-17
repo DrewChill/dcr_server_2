3d13b59d293a6ede720bce742724a7de6c0c09a4
DrewChill <dhill4554@gmail.com>
2018-06-17 14:43:18 -0600

    lot of updates. new repo

---
+++ byte_msg_parser.h
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   byte_msg_parser.h
+ * Author: drew
+ *
+ * Created on June 17, 2018, 9:32 AM
+ */
+
+#ifndef BYTE_MSG_PARSER_H
+#define BYTE_MSG_PARSER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+#include <stdlib.h>
+#include <stdint.h>
+
+#define CREATE_ROOM_TYPE 1
+#define CONNECT_TO_CONTAINER_TYPE 2
+#define PLAYER_CMND_TYPE 3
+#define REQUEST_STATUS_TYPE 50
+
+typedef uint16_t msg_type_t;
+
+typedef struct{
+	msg_type_t msg_type;
+	uint32_t user_id;
+	uint32_t room_id; //0 when creating room
+	size_t body_length;
+	//maybe some other stuff in the future
+} msg_header_t;
+
+typedef struct{
+	//uint32_t user_id;
+	//initial player state info probably
+} create_room_msg_t;
+
+typedef struct{
+	//uint32_t room_id;
+	//uint32_t user_id;
+	//maybe some other stuff in the future
+} connect_to_container_msg_t;
+
+typedef struct{
+	msg_header_t header;
+	uint8_t status;
+} request_status_msg_t;
+
+typedef struct{
+	
+} player_cmnd_msg_t;
+
+//will construct a request response message
+void construct_request_response_msg(request_status_msg_t *msg, uint8_t status, uint32_t user_id, uint32_t room_id);
+
+//will populate the message header info. returns success status
+int parse_header_info(char *full_msg, msg_header_t *header);
+
+//will populate the create room msg struct from the byte msg. returns parse success status
+int parse_create_room_msg(char *byte_msg, create_room_msg_t *new_msg);
+
+//will populate the connect msg from byte msg. returns the parse success status
+int parse_connect_to_container_msg(char *byte_msg, connect_to_container_msg_t *new_msg);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BYTE_MSG_PARSER_H */
+
