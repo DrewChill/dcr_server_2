33a5e107e8a5e59340625ac0a3e3deee43884f66
DrewChill <dhill4554@gmail.com>
2018-07-01 10:45:38 -0600

    chat messaging

--- distribution_container_manager.c
+++ distribution_container_manager.c
@@ -522,7 +522,7 @@ void handle_incoming_data(msg_header_t header, char *data){
         pthread_mutex_lock(&container->recv_data.recv_lock);
 
         int len = header.msg_length;
-        printf("filling ditribution data buffer...\n");fflush(stdout);
+        printf("filling ditribution data buffer @ %d...\n", container->recv_data.head);fflush(stdout);
         if(len+container->recv_data.head > BUFFER_LENGTH){
             int space_left = (BUFFER_LENGTH-container->recv_data.head);
             memcpy(container->recv_data.recv_buffer+container->recv_data.head, data, space_left);
